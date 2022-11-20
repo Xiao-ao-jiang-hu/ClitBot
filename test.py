74799fdd096c02bd254820b05bc97c987edd8a2f
校奥浆糊 <wst22@mails.tsinghua.edu.cn>
2022-11-20 18:07:18 +0800

    11.20 wst

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import cv2
 
-a = cv2.imread("resources/background.bmp")
+a = cv2.imread("outputs/output.bmp")
 cv2.imshow('a',a)
 cv2.waitKey(1000)
-a = cv2.imread("resources/single_rock.bmp")
+a = cv2.imread("outputs/output.bmp")
 cv2.imshow('a',a)
 cv2.waitKey(1000)
