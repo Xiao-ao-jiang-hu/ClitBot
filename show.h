c93135163960a0ca018725efc18ed2214b25b5f3
校奥浆糊 <wst22@mails.tsinghua.edu.cn>
2022-11-21 10:12:46 +0800

    11.21 wst

--- show.h
+++ show.h
@@ -1,55 +1,48 @@
-//#include<bits/stdc++.h>
-
-#include<Windows.h>
-#include<wingdi.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<iostream>
-using namespace std;
+#pragma once
+#include <windows.h>
+#include <stdio.h>
 
 
 /***********************************************************************
 在命令行中展示一张图片
 参数： filename（char*），要展示的图片的路径
 ***********************************************************************/
-void show(char* filename)
+int show(char* filename)
 {
-	//读取图像文件
-	FILE* fp;
-	errno_t err = fopen_s(&fp, filename, "rb");
-	//FILE* fp = fopen_s(&tmp, "outputs/output.bmp", "rb");//读入的文件
-	if (!fp) {
-		cout << "文件未打开！\n";
-		exit(0);
-	}
-	long width, height;
-	BITMAPFILEHEADER fileHead;//位图文件头
-	fread(&fileHead, sizeof(BITMAPFILEHEADER), 1, fp);//从fp中读取BITMAPFILEHEADER信息到fileHead中,同时fp的指针移动
-	BITMAPINFOHEADER infoHead;//位图信息头
-	fread(&infoHead, sizeof(BITMAPINFOHEADER), 1, fp);//从fp中读取BITMAPINFOHEADER信息到infoHead中,同时fp的指针移动
-	width = infoHead.biWidth;
-	height = infoHead.biHeight;
-
-
-	char* bmpBuf = new char[width * height * 3];
-	fseek(fp, long(sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)), 0);//定位到起始位置
-	fread(bmpBuf, sizeof(char), width * height * 3, fp);
-	fclose(fp);
-
-	//显示图像
-	HWND hwnd = GetForegroundWindow();//窗口句柄
-	HDC hdc = GetDC(hwnd);//绘图句柄
-	BYTE b, g, r;
-	char* tmp = bmpBuf;
-	for (int j = 0; j < height; j++) {
-		for (int i = 0; i < width; i++) {
-			b = *tmp++;
-			g = *tmp++;
-			r = *tmp++;
-			SetPixel(hdc, i, 150 + height - j, RGB(r, g, b));
-		}
+	FILE* file = NULL;
+	errno_t err = fopen_s(&file, filename, "rb"); // 只能显示bmp格式图片
+	if (!file)
+	{
+		printf("图片文件打开失败！\n");
+		return -1;
 	}
 
+	fseek(file, 0, SEEK_END);
+	size_t size = ftell(file);
+
+	LPBITMAPINFOHEADER bi = (LPBITMAPINFOHEADER)malloc(size);
+	if (!bi)
+	{
+		printf("内存分配失败！\n");
+		return -2;
+	}
+
+	fseek(file, sizeof(BITMAPFILEHEADER), SEEK_SET);
+	fread(bi, 1, size, file);
+
+	char Title[255] = { 0 };
+	GetConsoleTitleA(Title, 255);
+	HDC dc = GetDC(FindWindowA(0, Title));
+	SetConsoleTitleA("控制台显示图片");
+
+	do
+	{
+		SetDIBitsToDevice(dc, 50, 10, bi->biWidth, bi->biHeight, 0, -0, 0, bi->biHeight, bi + 1, (LPBITMAPINFO)bi, 0);
+	} while (printf("%c", getchar()));
+
+	getchar();
+	return 0;
 }
+
+
 
